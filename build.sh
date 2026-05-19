6fa24580b4754d6d32248fa138cb4e190d9f993a
lxxilyd <lixxing_lxf@foxmail.com>
2026-05-19 15:19:54 +0800

    feat: 支持stream模块

--- build.sh
+++ build.sh
@@ -43,13 +43,27 @@ RUN ./configure \
     --http-fastcgi-temp-path=temp/fastcgi \
     --http-uwsgi-temp-path=temp/uwsgi \
     --http-scgi-temp-path=temp/scgi \
+    --with-threads \
+    --with-file-aio \
+    --with-pcre \
+    --with-pcre-jit \
     --with-http_ssl_module \
     --with-http_v2_module \
-    --with-http_flv_module \
-    --with-http_stub_status_module \
+    --with-http_realip_module \
+    --with-http_addition_module \
+    --with-http_sub_module \
+    --with-http_gunzip_module \
     --with-http_gzip_static_module \
-    --with-pcre \
-    --with-cc-opt="-static -O3" \
+    --with-http_auth_request_module \
+    --with-http_random_index_module \
+    --with-http_secure_link_module \
+    --with-http_stub_status_module \
+    --with-http_slice_module \
+    --with-stream \
+    --with-stream_ssl_module \
+    --with-stream_realip_module \
+    --with-stream_ssl_preread_module \
+    --with-cc-opt="-static -O3 -fstack-protector-strong -fPIC" \
     --with-ld-opt="-static"
 
 RUN make -j$(nproc) && make install DESTDIR=/output
